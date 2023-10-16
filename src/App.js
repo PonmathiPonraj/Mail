530ae2065e458afd40af9a6cfcde971f0752147f
ponmathi <ponmathittl@gmail.com>
2023-10-16 19:56:29 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js';
+import { Route,Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SignUp from './components/SignUp/SignUp';
+import MailBox from './components/MailBox/mailBox';
+import MailboxEditor from './MailBoxEditor';
+import Inbox from './components/MailBox/inbox';
+import SentEmail from './components/MailBox/SentEmail';
 
 function App() {
+  const isLoggedin=useSelector((state)=>state.auth.isAuthenticated)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+
+      <Route path='/'>
+        {!isLoggedin && <Redirect to='/Login' />}
+      </Route>
+      <Route path='/Login'>
+        <SignUp />
+      </Route>
+      <Route path='/Mailbox'>{isLoggedin && <MailBox />}</Route>
+      <Route path='/ComposeMail'><MailboxEditor /></Route>
+      <Route path='/Inbox'><Inbox /></Route>
+      <Route path='/Sent'><SentEmail /></Route>
     </div>
   );
 }
